92fd776930e702d88d10f3ad55757dcacd5e5431
lshinjikashvili <lshinjikashvili@crocobet.com>
2023-12-28 21:59:27 +0400

    feat:changed minor things

--- src/app/ui/specific-blog/specific-blog.component.ts
+++ src/app/ui/specific-blog/specific-blog.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CategoryService} from "../../services/category.service";
 import {map, Observable, of, switchMap, take} from "rxjs";
 import {Post, SpecificPost} from "../../models/blog";
@@ -17,11 +17,19 @@ import {CardComponent} from "../card/card.component";
 export class SpecificBlogComponent implements OnInit{
   public specificBlog$!:Observable<SpecificPost>;
   public similarBlogs$!:Observable<Post[]>
-constructor(private categoryService:CategoryService,  private route: ActivatedRoute) {
+  public displayLeftArrow2 = false;
+  public displayRightArrow2 = true;
+  protected readonly SCROLL_STEP_2 = 100;
+  @ViewChild('swiperWrapper') swiperWrapper: any;
+  @ViewChild('swiperContent') swiperContent: any;
+
+  constructor(private categoryService:CategoryService,  private route: ActivatedRoute) {
 }
 
 ngOnInit() {
   const blogId :number = +this.route.snapshot.params['id'];
+
+
   this.specificBlog$ = this.categoryService.getByIdBlogs(blogId).pipe(map((res:any) => {
     return res
     }))
@@ -34,6 +42,32 @@ ngOnInit() {
   })
 
 }
+  handleArrow2(arrow: 'left' | 'right') {
+    const viewContainerElement = this.swiperContent
+      .nativeElement as HTMLElement;
 
-  protected readonly Array = Array;
+    if (arrow === 'left') {
+      viewContainerElement.scrollLeft -= this.SCROLL_STEP_2;
+      this.displayRightArrow2 = true;
+    } else {
+      viewContainerElement.scrollLeft +=  this.SCROLL_STEP_2;
+    }
+
+    if (viewContainerElement.scrollLeft) {
+      this.displayLeftArrow2 = true;
+    }
+
+    if (
+      viewContainerElement.clientWidth + viewContainerElement.scrollLeft >=
+      viewContainerElement.scrollWidth
+    ) {
+      this.displayRightArrow2 = false;
+      this.displayLeftArrow2 = true;
+    } else if (!viewContainerElement.scrollLeft) {
+      this.displayRightArrow2 = true;
+      this.displayLeftArrow2 = false;
+    }
+
+  }
+
 }
